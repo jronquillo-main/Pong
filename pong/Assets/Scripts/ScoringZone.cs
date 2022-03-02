766cb12036d3f7466d819b713150c56096d9eca5
Justin <ronquillojustindavidv@gmail.com>
2022-03-02 08:09:44 +0800

    Final

---
+++ pong/Assets/Scripts/ScoringZone.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class ScoringZone : MonoBehaviour
+{
+    public EventTrigger.TriggerEvent scoreTrigger;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+
+        if(ball != null)
+        {
+            BaseEventData eventData = new BaseEventData(EventSystem.current);
+            this.scoreTrigger.Invoke(eventData);
+        }
+    }
+}
